70858b1c09a8e68935066b638fc8de8da077570a
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-28 16:24:22 +0200

    3.1 Inkapsling
    Skapa klass Person.
    Skapa instans av klass Person.

---
+++ Ovning3/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning3
+{
+    internal class Person
+    {
+        private int age;
+        private string fName = string.Empty;
+        private string lName= string.Empty;
+        private double height;
+        private double weight;
+
+        public int Age
+        { 
+            get { return age; } 
+            set {  age = value; } 
+        }
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        public string LName
+        { 
+            get { return lName; } 
+            set { lName = value; } 
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+    }
+}
--- Ovning3/Program.cs
+++ Ovning3/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person jane = new Person();
+            jane.LName = "Pettersson";
+            Console.WriteLine(jane.FName);//""
+            Console.WriteLine(jane.LName);//Pettersson
         }
     }
 }
