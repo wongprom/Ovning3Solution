f48a4e2b45a29ef848bd30ca54243c539350adb5
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-30 11:50:53 +0200

    Abstract class, virtual, override inherit
    Create abstract class UseError
    Create other classes that inherit from UseError and override method UEMessage.

---
+++ Ovning3/TextInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning3
+{
+    internal class TextInputError : UseError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error!";
+        }
+    }
+}
---
+++ Ovning3/UseError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning3
+{
+    abstract class UseError
+    {
+        public virtual string UEMessage()
+        {
+            return "return some string";
+        }
+    }
+}
