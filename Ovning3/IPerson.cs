fbb9640749d7c35d245edf0f684243c1c773eccd
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-30 16:50:34 +0200

    Create IPerson interface.

---
+++ Ovning3/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning3
+{
+    internal interface IPerson
+    {
+        public void Talk()
+        {
+
+        }
+    }
+}
