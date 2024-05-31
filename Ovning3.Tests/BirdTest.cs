9a959f4b638584c7ed1a418c91bc9be353cc57fd
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-31 18:21:46 +0200

    BirdTest WIP

---
+++ Ovning3.Tests/BirdTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Ovning3.Tests
+{
+    public class BirdTest
+    {
+        [Fact]
+        public void InitiateClass_ShouldGetvaluesNameAgeWeightWingSpan()
+        {
+          /*  //Arrange
+            var bird = new Bird();
+            const string expected = "return some string";
+
+            //Act
+            var result = useError.UEMessage();
+
+            //Assert
+            Assert.True(result == expected);
+        }*/
+    }
+}
