666c3033c64b8289f7c49fa09a50823ba234ce73
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-31 11:05:57 +0200

    Create animalList and add different type of animals.
    call DoSound for each animal.
    what happends with wolfman...?

--- Ovning3/Program.cs
+++ Ovning3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Ovning3
+﻿using System.Collections.Generic;
+
+namespace Ovning3
 {
     internal class Program
     {
@@ -45,11 +47,33 @@
                      Console.Write(print.PrintThis());
                  }*/
 
-                Wolfman wolf = new Wolfman("jimmy", 34, 34, true);
-                wolf.Talk();
-
-                List<Animal> animals = new List<Animal>();
+                List<Animal> animalsList = new List<Animal>();
+                Wolfman wolfMan = new Wolfman("jimmy", 34, 34, true);
                 Swan svan = new Swan("Svan", 23, 3, 2, true);
+                Flamingo flamingo = new Flamingo("flam", 1, 2, 3, false);
+                Wolf wolf = new Wolf("wolf", 3, 200, true);
+                Pelican pelican = new Pelican("pelican", 3, 3, 4,false);
+                Bird bird = new Bird("Bird", 3, 4, 2);
+                Worm worm = new Worm("worm", 3, 34, false);
+                Hedgehog hedgehog = new Hedgehog("Hedgehog", 3, 45, 2);
+                Dog dog = new Dog("dog", 3, 3,false);
+                Horse horse = new Horse("Horse", 2,2,true);
+                animalsList.Add(wolfMan);
+                animalsList.Add(worm);
+                animalsList.Add(svan);
+                animalsList.Add(flamingo);
+                animalsList.Add(wolf);
+                animalsList.Add(bird);
+                animalsList.Add(pelican);
+                animalsList.Add(hedgehog);
+                animalsList.Add(dog); 
+                animalsList.Add(horse);
+
+                foreach (Animal animal in animalsList)
+                {
+                    Console.WriteLine(animal);
+                    animal.DoSound();
+                }
 
             }
             catch (ArgumentException ex)
